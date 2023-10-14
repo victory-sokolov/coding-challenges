0f5be6de372b4209821632dd5c672fb24e7361e0
Viktor Sokolov <viktorsokolov.and@gmail.com>
2023-10-14 13:59:17 +0300

    Add link to taask

--- Python/two-sum.py
+++ Python/two-sum.py
@@ -1,3 +1,4 @@
+# Task: https://leetcode.com/problems/two-sum/description/
 def twoSum(self, nums: list[int], target: int) -> list[int]:
     for index, num in enumerate(nums):
         for index2, num2 in enumerate(nums[index+1:]):

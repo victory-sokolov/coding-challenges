60d6e2a7e20bbe65e7f9a4fcd5a383cc4047ff61
Viktor Sokolov <viktorsokolov.and@gmail.com>
2024-03-28 10:44:01 +0200

    update two sum implementation

--- Python/two-sum.py
+++ Python/two-sum.py
@@ -1,6 +1,26 @@
 # Task: https://leetcode.com/problems/two-sum/description/
-def twoSum(self, nums: list[int], target: int) -> list[int]:
+# O(n^2) algorithmic complexity
+def two_sum(nums: list[int], target: int) -> list[int]:
+    total = len(nums)
+
+    for i, num in enumerate(nums):
+        for j in range(i + 1, total):
+            sum = num + nums[j]
+            if sum == target:
+                return [i, j]
+
+    return []
+
+
+def two_sum_hash_map(nums: list[int], target: int) -> list[int]:
+    hash_map = {}
+
     for index, num in enumerate(nums):
-        for index2, num2 in enumerate(nums[index+1:]):
-            if num + num2 == target:
-                return [index, index+1]
+        diff = target - num
+
+        if diff in hash_map:
+            return [hash_map[diff], index]
+
+        hash_map[num] = index
+
+    return []
